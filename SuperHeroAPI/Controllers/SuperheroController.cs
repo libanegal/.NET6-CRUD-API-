0bc6c775c7adcbc9e1fa4929aac2d08a1ed25920
DESKTOP-RU2QNRI\Sony <libanegal715@gmail.com>
2023-05-04 22:49:24 +0100

    Superhero Api. Put function, create function, update function

---
+++ SuperHeroAPI/Controllers/SuperheroController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperheroController : ControllerBase
+    {
+        private static List<Superhero> heroes = new List<Superhero>
+            {
+                new Superhero
+                {
+                    Id = 1,
+                    Name = "SPider Man",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York City"
+                },
+                 new Superhero
+                {
+                    Id = 2,
+                    Name = "Ironman",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Long Island"
+                }
+
+            };
+        [HttpGet]
+        public async Task<ActionResult<List<Superhero>>> Get()
+        {
+            
+            return Ok(heroes);
+ 
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Superhero>>> Get(int id)
+        {
+
+            var hero = heroes.Find(h => h.Id == id);
+            if (hero == null) 
+            {
+                return BadRequest("Request not found");
+            }
+            return Ok(hero);
+
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Superhero>>> AddHero(Superhero hero)
+        {
+            heroes.Add(hero);
+            return Ok(heroes);
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Superhero>>> UpdateHero(Superhero request)
+        {
+            var hero = heroes.Find(h => h.Id == request.Id);
+            if (hero == null)
+            {
+                return BadRequest("Request not found");
+            }
+            heroes.Add(hero);
+            return Ok(heroes);
+
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.FirstName;
+            hero.Place = request.Place;
+        }
+    }
+}
